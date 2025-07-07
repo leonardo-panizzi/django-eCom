8c2281de77aefe3658dfb610b29d0c90bbdbf4db
leonardo-panizzi <leonardo.panizzi@edu.unifi.it>
2025-07-07 16:20:04 +0200

    enhance media URL handling for development and production environments

--- ecom/urls.py
+++ ecom/urls.py
@@ -11,3 +11,15 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.views.static import serve
+    from django.urls import re_path
+
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
